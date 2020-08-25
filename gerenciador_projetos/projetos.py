6f31e2e9c34a4c3ff7af20f2c85f0abea37121ba
Vitor Pires <vitor.pires@thoughtworks.com>
2020-08-25 10:53:26 -0300

    Altera para emails genéricos

--- gerenciador_projetos/projetos.py
+++ gerenciador_projetos/projetos.py
@@ -9,28 +9,28 @@ PROJETOS = [
         "nome": "Projeto exemplo 1",
         "descricao": "Descrição de exemplo, Descrição de exemplo",
         "escritorio": "BH",
-        "responsavel": "vitor.pires@thoughtworks.com"
+        "responsavel": "vitor.pires@email.com"
     },
         {
         "id": 2,
         "nome": "Projeto exemplo 2",
         "descricao": "Descrição de exemplo, Descrição de exemplo",
         "escritorio": "SP",
-        "responsavel": "wilton.garcia@thoughtworks.com"
+        "responsavel": "wilton.garcia@email.com"
     },
     {
         "id": 3,
         "nome": "Projeto exemplo 3",
         "descricao": "Descrição de exemplo, Descrição de exemplo",
         "escritorio": "POA",
-        "responsavel": "wilton.garcia@thoughtworks.com"
+        "responsavel": "wilton.garcia@email.com"
     },
        {
         "id": 4,
         "nome": "Projeto exemplo 4",
         "descricao": "Descrição de exemplo, Descrição de exemplo",
         "escritorio": "Recife",
-        "responsavel": "vitor.pires@thoughtworks.com"
+        "responsavel": "vitor.pires@email.com"
     }
 ]
 
