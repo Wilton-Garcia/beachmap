ff7230c50cef2edb950e4aec03241da6682fde11
Vitor Pires <vitor.pires@thoughtworks.com>
2020-08-26 10:28:40 -0300

    Criando projeto no xcode

---
+++ BeachMapMobile/BeachMapMobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeachMapMobile
+//
+//  Created by LAVitor Pires on 26/08/20.
+//  Copyright © 2020 LAVitor Pires. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
