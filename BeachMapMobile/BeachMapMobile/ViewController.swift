560dba78d03e80f960812764a8dcdf2bcbd6d887
Vitor Pires <vitor.pires@thoughtworks.com>
2020-09-01 11:03:21 -0300

    Aplicando melhoria no layout

--- BeachMapMobile/BeachMapMobile/ViewController.swift
+++ BeachMapMobile/BeachMapMobile/ViewController.swift
@@ -11,9 +11,9 @@ import UIKit
 class ViewController: UIViewController{
     
     let projetos = [
-        Projeto(id: 0, nome: "Projeto Exemplo 1", escritorio: "Belo Horizonte", descricao: "orem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonIpslon", responsaveis: "vitor.prites@email.com"),
-        Projeto(id: 1, nome: "Projeto Exemplo 2", escritorio: "Recife", descricao: "Lorem Ipslon Lorem Lorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLn", responsaveis: "wilton.garcia@email.com")
-    
+        Projeto(id: 0, nome: "Projeto Exemplo 1", escritorio: "Belo Horizonte", descricao: "orem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon ", responsaveis: "vitor.prites@email.com,  wilton.garcia@email.com, wilton.garcia@email.com"),
+        Projeto(id: 1, nome: "Projeto Exemplo 2", escritorio: "Recife", descricao: "Lorem Ipslon Lorem Lorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem IpslonLorem Ipslon Lorem ", responsaveis: "wilton.garcia@email.com")
+        
     ]
     
     @IBOutlet weak var tabelaProjetos: UITableView!
@@ -24,8 +24,8 @@ class ViewController: UIViewController{
         tabelaProjetos.delegate = self
         tabelaProjetos.dataSource = self
     }
-
-
+    
+    
 }
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate{
@@ -37,17 +37,35 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customcell") as! ProjetosTableViewCell
         
-        cell.Nome.text = projetos[indexPath.row].nome
-        cell.Escritorio.text = projetos[indexPath.row].escritorio
-        cell.Descricao.text = projetos[indexPath.row].descricao
-        cell.Responsaveis.text = projetos[indexPath.row].responsaveis
+        let projeto = projetos[indexPath.row]
+        let stringResponsaveisProjetosFormatada = formataStringResponsaveis(responsaveis: projeto.responsaveis)
+        
+        cell.Nome.text = projeto.nome
+        cell.Escritorio.text = projeto.escritorio
+        cell.Descricao.text = projeto.descricao
+        cell.Responsaveis.attributedText = stringResponsaveisProjetosFormatada
         
         return cell
-    
+        
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 240
+        return 200
     }
+    
+    func formataStringResponsaveis(responsaveis: String) -> NSAttributedString {
+        
+        let boldText = "Responsáveis: "
+        let attributsBold = [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16, weight: .bold)]
+        let attributsNormal = [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16, weight: .regular)]
+        let attributedString = NSMutableAttributedString(string: responsaveis, attributes:attributsNormal)
+        let boldStringPart = NSMutableAttributedString(string: boldText, attributes:attributsBold)
+        
+        boldStringPart.append(attributedString)
+        
+        return boldStringPart
+        
+    }
+    
 }
 
