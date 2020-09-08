7ff15d1f7a590bf3a7fd4ea1f8e5764958bad386
Wilton Garcia <wiltongarciaweb@gmail.com>
2020-09-08 15:51:58 -0300

    Adicionando mais dados para a API retornar

--- gerenciador_projetos/projetos.py
+++ gerenciador_projetos/projetos.py
@@ -6,39 +6,40 @@ app = FastAPI()
 PROJETOS = [
     {
         "id": 1,
-        "nome": "Projeto exemplo 1",
-        "descricao": "Descrição de exemplo, Descrição de exemplo",
+        "nome": "Beach Map",
         "escritorio": "BH",
-        "responsavel": "vitor.pires@email.com"
+        "descricao": "Atualmente tem pessoas na praia que querem ter alguma experiência prática em um projeto e experimentar novas tecnologias e metodologias. O objetivo do BeachMap é conectar essas pessoas a projetos de praia disponíveis, ajudá-los a se desenvolver e contribuir para o lançamento de novos projetos incríveis. Além disso, o BeachMap tem o objetivo de centralizar e tornar visíveis os projetos que de fato estão acontecendo, que antes eram gerenciados por formulários e distribuídos pelas praias dos escritórios.",
+        "desafios":"Integração com Okta; Integração com Jigsaw; Integração com CircleCI; Praticar testes automatizados; Praticar Redux; Cumprir os critérios de segurança estabelecidos pelo time de TechOps",
+        "techStack":"* React \n * Heroku \n* Jest \n* React Testing Library",
+        "oportunidade":["DEV", "XD"],
+        "status":"emProgresso",
+        "dataUltimaAtualizacao":"01/01/2001",
+        "responsaveis": "vitor.pires@email.com"
     },
-        {
+ {
         "id": 2,
-        "nome": "Projeto exemplo 2",
-        "descricao": "Descrição de exemplo, Descrição de exemplo",
-        "escritorio": "SP",
-        "responsavel": "wilton.garcia@email.com"
+        "nome": "Beach Map",
+        "escritorio": "BH",
+        "descricao": "Atualmente tem pessoas na praia que querem ter alguma experiência prática em um projeto e experimentar novas tecnologias e metodologias. O objetivo do BeachMap é conectar essas pessoas a projetos de praia disponíveis, ajudá-los a se desenvolver e contribuir para o lançamento de novos projetos incríveis. Além disso, o BeachMap tem o objetivo de centralizar e tornar visíveis os projetos que de fato estão acontecendo, que antes eram gerenciados por formulários e distribuídos pelas praias dos escritórios.",
+        "desafios":"Integração com Okta; Integração com Jigsaw; Integração com CircleCI; Praticar testes automatizados; Praticar Redux; Cumprir os critérios de segurança estabelecidos pelo time de TechOps",
+        "techStack":"* React \n * Heroku \n* Jest \n* React Testing Library",
+        "oportunidade":["DEV", "XD"],
+        "status":"emProgresso",
+        "dataUltimaAtualizacao":"01/01/2001",
+        "responsaveis": "vitor.pires@email.com"
     },
-    {
+ {
         "id": 3,
-        "nome": "Projeto exemplo 3",
-        "descricao": "Descrição de exemplo, Descrição de exemplo",
-        "escritorio": "POA",
-        "responsavel": "wilton.garcia@email.com"
+        "nome": "Beach Map",
+        "escritorio": "BH",
+        "descricao": "Atualmente tem pessoas na praia que querem ter alguma experiência prática em um projeto e experimentar novas tecnologias e metodologias. O objetivo do BeachMap é conectar essas pessoas a projetos de praia disponíveis, ajudá-los a se desenvolver e contribuir para o lançamento de novos projetos incríveis. Além disso, o BeachMap tem o objetivo de centralizar e tornar visíveis os projetos que de fato estão acontecendo, que antes eram gerenciados por formulários e distribuídos pelas praias dos escritórios.",
+        "desafios":"Integração com Okta; Integração com Jigsaw; Integração com CircleCI; Praticar testes automatizados; Praticar Redux; Cumprir os critérios de segurança estabelecidos pelo time de TechOps",
+        "techStack":"* React \n * Heroku \n* Jest \n* React Testing Library",
+        "oportunidade":["DEV", "XD"],
+        "status":"emProgresso",
+        "dataUltimaAtualizacao":"01/01/2001",
+        "responsaveis": "vitor.pires@email.com"
     },
-       {
-        "id": 4,
-        "nome": "Projeto exemplo 4",
-        "descricao": "Descrição de exemplo, Descrição de exemplo",
-        "escritorio": "Recife",
-        "responsavel": "vitor.pires@email.com"
-    },
-        {
-        "id": 5,
-        "nome": "Projeto exemplo 4",
-        "descricao": "Descrição de exemplo, Descrição de exemplo",
-        "escritorio": "Recife",
-        "responsavel": "vitor.pires@email.com"
-    }
 ]
 
 @app.get("/projetos")
