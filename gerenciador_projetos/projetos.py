4b84e4f8cd67675df0dd92be32686647e4b50a86
Wilton Garcia <wiltongarciaweb@gmail.com>
2020-09-09 10:17:42 -0300

    WIP Alterando dados da API

--- gerenciador_projetos/projetos.py
+++ gerenciador_projetos/projetos.py
@@ -16,9 +16,9 @@ PROJETOS = [
         "dataUltimaAtualizacao":"01/01/2001",
         "responsaveis": "vitor.pires@email.com"
     },
- {
+    {
         "id": 2,
-        "nome": "Beach Map",
+        "nome": "Beach Map 2",
         "escritorio": "BH",
         "descricao": "Atualmente tem pessoas na praia que querem ter alguma experiência prática em um projeto e experimentar novas tecnologias e metodologias. O objetivo do BeachMap é conectar essas pessoas a projetos de praia disponíveis, ajudá-los a se desenvolver e contribuir para o lançamento de novos projetos incríveis. Além disso, o BeachMap tem o objetivo de centralizar e tornar visíveis os projetos que de fato estão acontecendo, que antes eram gerenciados por formulários e distribuídos pelas praias dos escritórios.",
         "desafios":"Integração com Okta; Integração com Jigsaw; Integração com CircleCI; Praticar testes automatizados; Praticar Redux; Cumprir os critérios de segurança estabelecidos pelo time de TechOps",
@@ -30,7 +30,7 @@ PROJETOS = [
     },
  {
         "id": 3,
-        "nome": "Beach Map",
+        "nome": "Beach Map 3",
         "escritorio": "BH",
         "descricao": "Atualmente tem pessoas na praia que querem ter alguma experiência prática em um projeto e experimentar novas tecnologias e metodologias. O objetivo do BeachMap é conectar essas pessoas a projetos de praia disponíveis, ajudá-los a se desenvolver e contribuir para o lançamento de novos projetos incríveis. Além disso, o BeachMap tem o objetivo de centralizar e tornar visíveis os projetos que de fato estão acontecendo, que antes eram gerenciados por formulários e distribuídos pelas praias dos escritórios.",
         "desafios":"Integração com Okta; Integração com Jigsaw; Integração com CircleCI; Praticar testes automatizados; Praticar Redux; Cumprir os critérios de segurança estabelecidos pelo time de TechOps",
