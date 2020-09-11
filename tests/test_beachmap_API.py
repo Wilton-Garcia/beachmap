7e577e0858fedbbad3bf8d33f4e18d7a59915928
Jean <jeanmacena@gmail.com>
2020-09-11 11:03:35 -0300

    Teste python api

--- tests/test_beachmap_API.py
+++ tests/test_beachmap_API.py
@@ -20,7 +20,14 @@ def test_quando_listar_projetos_retorno_deve_ser_uma_lista():
     resposta = cliente.get("/projetos")
     assert isinstance(resposta.json(), list)
 
-def test_quando_listar_projetos_retorno_deve_ser_uma_lista2():
+def test_quando_submeter_projeto_deve_ter_titulo():
     cliente = TestClient(app)
-    resposta = cliente.get("/projetos")
-    assert isinstance(resposta.json(), list)
+    resposta = cliente.post("/projeto", json={})
+    assert resposta.status_code == 422
+
+def test_nome_projeto_deve_conter_entre_3_e_100_caracteres():
+    client = TestClient(app)
+    resposta = client.post("/projeto", json={"nome_projeto": 2 * "*"})
+    assert resposta.status_code == 422
+    resposta = client.post("/projeto", json={"nome_projeto": 101 * "*"})
+    assert resposta.status_code == 422
