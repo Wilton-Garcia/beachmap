d0a73d178eed3ab423be8187125165e14c7f1bc0
Wilton Garcia <wiltongarciaweb@gmail.com>
2020-09-22 16:11:39 -0300

    Adicionado dependendcias no arquivo de dev-requirements no caminho correto

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: uvicorn --host=0.0.0.0 --port=$PORT gerenciador_projetos.projetos:app
-
--- run.sh
+++
@@ -1,4 +0,0 @@
-pipenv shell
-python3 -m pytest
-cd gerenciador_projetos
-uvicorn --reload gerenciador_projetos.projetos:app
